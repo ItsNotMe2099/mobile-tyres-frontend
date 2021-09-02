98772eddd86df543443aad8f70b13493e971e681
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2021-09-02 10:31:43 +0300

    Price component

---
+++ components/Price/index.tsx
@@ -0,0 +1,35 @@
+import styles from './index.module.scss'
+
+
+interface Props {
+
+}
+
+export default function Price(props: Props) {
+
+  const options = [{label: 'Стоимость 1км выезда', price: '350 руб.'}, {label: 'Ремонт дисков', price: '3 000 руб.'},
+  {label: 'Срочный ремонт', price: '5 350 руб.'}, {label: 'Ремонт за пределами Москвы', price: '2 250 руб.'},
+  {label: 'Переобувка', price: '850 руб.'}, {label: 'Хранение шин', price: '6 550 руб.'},
+  {label: 'Выезд в ночное время', price: '550 руб.'}, {label: 'Ремонт шин', price: '1 500 руб.'},
+  {label: 'Ремон в пределах Москвы', price: '2 000 руб.'}]
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>
+        Средняя стоимость услуг
+      </div>
+      <div className={styles.prices}>
+        {options.map(item =>
+          <div className={styles.option}>
+            <div className={styles.label}>
+              {item.label}
+            </div>
+            <div className={styles.price}>
+              {item.price}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Footer from 'components/layout/Footer'
 import Header from 'components/layout/Header'
 import Layout from 'components/layout/Layout'
 import Main from 'components/Main'
+import Price from 'components/Price'
 import styles from './index.module.scss'
 
 export default function Home(props) {
@@ -14,6 +15,7 @@ export default function Home(props) {
       <Main/>
       <HowItWorks/>
       <Advantages/>
+      <Price/>
       </body>
       <AboutUs/>
     </Layout>
